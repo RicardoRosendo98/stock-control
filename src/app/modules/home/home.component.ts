69fce527ebaf6274d04f31707da88032668b9b2c
Ricardo Rosendo <ricardorosendo98@hotmail.com>
2025-01-18 14:17:13 -0300

    melhorando o css e implementando a validação na tela home

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -7,6 +7,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user/user.service';
 import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -31,7 +32,8 @@ export class HomeComponent {
     private formBuilder: FormBuilder,
     private userService: UserService,
     private cookieService: CookieService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private router: Router
   ) {}
 
   onSubmitLoginForm(): void {
@@ -42,6 +44,7 @@ export class HomeComponent {
           if (response) {
             this.cookieService.set('USER_INFO',response?.token);
             this.loginForm.reset();
+            this.router.navigate(['/dashboard']);
 
             this.messageService.add({
               severity: 'success',
