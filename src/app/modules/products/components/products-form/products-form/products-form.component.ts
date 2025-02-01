ae6cd96f45f0278f7f69540fb978b2dd22d19cf2
Ricardo Rosendo <ricardorosendo98@hotmail.com>
2025-01-31 22:46:45 -0300

    adicionando a função de editar produto

--- src/app/modules/products/components/products-form/products-form/products-form.component.ts
+++ src/app/modules/products/components/products-form/products-form/products-form.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
+
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
-import { Subject, takeUntil } from 'rxjs';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+
 import { ProductsService } from './../../../../../services/products/products.service';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/GetCategoriesResponse';
 import { CreateProductRequest } from 'src/app/models/interfaces/products/request/CreateProductRequest';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
+import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
+import { ProductEvent } from 'src/app/models/enums/products/ProductEvent';
+import { EditProductResquest } from 'src/app/models/interfaces/products/request/EditProductRequest';
 
 
 @Component({
@@ -18,6 +26,12 @@ export class ProductsFormComponent implements OnInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject();
   public categoriesDatas: Array<GetCategoriesResponse> = []
   public selectedCategory: Array<{name: string; code: string}> = []
+  public productAction!: {
+    event: EventAction;
+    productDatas: Array<GetAllProductsResponse>;
+  };
+  public productSelectedDatas!: GetAllProductsResponse;
+  public productsDatas: Array<GetAllProductsResponse> = [];
   public addProductForm = this.formBuilder.group ({
     name: ['', Validators.required],
     price: ['', Validators.required],
@@ -26,16 +40,41 @@ export class ProductsFormComponent implements OnInit, OnDestroy {
     amount: [0, Validators.required],
   })
 
+  public editProductForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    description: ['', Validators.required],
+    amount: [0, Validators.required],
+  });
+
+
+  public addProductAction = ProductEvent.ADD_PRODUCT_EVENT;
+  public editProductAction = ProductEvent.EDIT_PRODUCT_EVENT;
+  public saleProductAction = ProductEvent.SALE_PRODUCT_EVENT;
+
+
   constructor(
     private categoriesService: CategoriesService,
     private productsService: ProductsService,
+    private productsDtService: ProductsDataTransferService,
     private formBuilder: FormBuilder,
     private messageService: MessageService,
     private router: Router,
+    public ref: DynamicDialogConfig,
 
   ) {}
 
   ngOnInit(): void {
+    this.productAction = this.ref.data;
+
+    if(this.productAction?.event?.action === this.editProductAction && this.productAction?.productDatas){
+      this.getProductSelecteddatas(this.productAction?.event?.id as string)
+    }
+
+    this.productAction?.event?.action === this.saleProductAction &&
+      this.getProductDatas()
+
+
     this.getAllCategories();
   }
   getAllCategories(): void {
@@ -86,6 +125,82 @@ export class ProductsFormComponent implements OnInit, OnDestroy {
     }
 
     this.addProductForm.reset();
+  }
+
+  handleSubmiteEditProduct(): void {
+    if (
+      this.editProductForm.value &&
+      this.editProductForm.valid &&
+      this.productAction.event.id
+    ) {
+      const requestEditproduct: EditProductResquest = {
+        name: this.editProductForm.value.name as string,
+        price: this.editProductForm.value.price as string,
+        description: this.editProductForm.value.description as string,
+        product_id: this.productAction?.event?.id,
+        amount: this.editProductForm.value.amount as number,
+      };
+
+      this.productsService
+      .editProduct(requestEditproduct)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Sucesso',
+            detail: 'Produto editado com sucesso!',
+            life: 2500,
+          });
+          this.editProductForm.reset();
+        }, error: (err) => {
+          console.log(err)
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'erro ao editar produto',
+            life: 2500,
+          });
+          this.editProductForm.reset();
+        }
+      })
+    }
+  }
+
+  getProductSelecteddatas(productId: string): void {
+    const allProducts = this.productAction?.productDatas;
+
+    if (allProducts.length > 0) {
+      const productFiltered = allProducts.filter(
+        (element) => element?.id === productId
+      );
+
+      if (productFiltered) {
+        this.productSelectedDatas = productFiltered[0];
+
+        this.editProductForm.setValue({
+          name: this.productSelectedDatas?.name,
+          price: this.productSelectedDatas?.price,
+          amount: this.productSelectedDatas?.amount,
+          description: this.productSelectedDatas?.description,
+        });
+      }
+    }
+  }
+
+
+  getProductDatas(): void {
+    this.productsService.getAllProducts()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        if (response.length > 0) {
+          this.productsDatas = response;
+          this.productsDatas &&
+          this.productsDtService.setProductsDatas(this.productsDatas);
+        }
+      }
+    })
   }
 
   ngOnDestroy(): void {
