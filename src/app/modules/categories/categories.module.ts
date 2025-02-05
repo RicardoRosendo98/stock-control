074d866531620c91d0e544c61002f9f7a6f18ee3
Ricardo Rosendo <ricardorosendo98@hotmail.com>
2025-02-05 20:33:05 -0300

    criando a ação de deletar e adicionar categorias

---
+++ src/app/modules/categories/categories.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CategoriesHomeComponent } from './page/categories-home/categories-home.component';
+import { CATEGORIES_ROUTES } from './categories.routing';
+
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { CategoriesTableComponent } from './components/categories-table/categories-table.component';
+import { SharedModule } from "../../shared/shared.module";
+import { CategoryFormComponent } from './components/category-form/category-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    CategoriesHomeComponent,
+    CategoriesTableComponent,
+    CategoryFormComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(CATEGORIES_ROUTES),
+    SharedModule,
+    HttpClientModule,
+    // PrimeNg
+    CardModule,
+    ButtonModule,
+    TableModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    DynamicDialogModule,
+    DropdownModule,
+    ConfirmDialogModule,
+    TooltipModule,
+],
+  providers: [DialogService, ConfirmationService]
+})
+export class CategoriesModule { }
